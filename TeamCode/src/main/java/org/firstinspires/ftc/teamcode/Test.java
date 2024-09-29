68abe73c6a156763e3337d4716712fd0da086d3c
Zirui-Ma <ziruima@outlook.com>
2024-09-29 13:22:31 -0600

    Merge remote-tracking branch 'origin/master_Zirui' into master_Zirui

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -1,47 +1,58 @@
-﻿package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Gamepad;
 
-public class Test {
+@TeleOp(name= "Test_Zirui", group = "OpMode")
+public class Test extends LinearOpMode {
     public DcMotor frontLeftMotor;
     public DcMotor frontRightMotor;
     public DcMotor backLeftMotor;
     public DcMotor backRightMotor;
 
-    public Gamepad gamepad1;
+    @Override
+    public void runOpMode() {
+        frontLeftMotor = hardwareMap.get(DcMotor.class, "FL_Motor");
+        frontRightMotor = hardwareMap.get(DcMotor.class, "FR_Motor");
+        backLeftMotor = hardwareMap.get(DcMotor.class, "BL_Motor");
+        backRightMotor = hardwareMap.get(DcMotor.class, "BR_Motor");
 
-    public Test(HardwareMap hardwaremap) {
-        frontLeftMotor = hardwaremap.get(DcMotor.class, "frontLeftMotor");
-        frontRightMotor = hardwaremap.get(DcMotor.class, "frontRightMotor");
-        backLeftMotor = hardwaremap.get(DcMotor.class, "backLeftMotor");
-        backRightMotor = hardwaremap.get(DcMotor.class, "backRightMotor");
+        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-    }
-    // Method to initialize the gamepad
-    public void initGamepad(Gamepad gamepad) {
-        this.gamepad1 = gamepad;
-    }
+        telemetry.addLine("Robot Initialized");
+        telemetry.update();
 
-    public void drive() {
-        double y = -gamepad1.left_stick_y;
-        double x = gamepad1.left_stick_x;
-        double rx = gamepad1.right_stick_x;
+        waitForStart();
 
-        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        while (opModeIsActive()) {
+            double y = -gamepad1.right_stick_y;
+            double x = gamepad1.right_stick_x;
+            double rx = gamepad1.left_stick_x;
 
-        double frontLeftMotorPower = (y + x + rx) / denominator;
-        double frontRightMotorPower = (y - x + rx) / denominator;
-        double backLeftMotorPower = (y - x - rx) / denominator;
-        double backRightMotorPower = (y + x - rx) / denominator;
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
 
-        frontLeftMotor.setPower(frontLeftMotorPower);
-        frontRightMotor.setPower(frontRightMotorPower);
-        backLeftMotor.setPower(backLeftMotorPower);
-        backRightMotor.setPower(backRightMotorPower);
+            double frontLeftMotorPower = (y + x + rx * 0.7) / denominator;
+            double frontRightMotorPower = (y - x - rx * 0.7) / denominator;
+            double backLeftMotorPower = (y - x + rx * 0.7)/ denominator;
+            double backRightMotorPower = (y + x - rx * 0.7) / denominator;
+
+            frontLeftMotor.setPower(frontLeftMotorPower);
+            frontRightMotor.setPower(frontRightMotorPower);
+            backLeftMotor.setPower(backLeftMotorPower);
+            backRightMotor.setPower(backRightMotorPower);
+
+            telemetry.addLine("Robot Initialized");
+            telemetry.addData("Front Left Power", frontLeftMotor.getPower());
+            telemetry.addData("Front Right Power", frontRightMotor.getPower());
+            telemetry.addData("Back Left Power", backLeftMotor.getPower());
+            telemetry.addData("Back Right Power", backRightMotor.getPower());
+            telemetry.addData("Joystick x", x);
+            telemetry.addData("Joystick y", y);
+            telemetry.addData("Joystick rx", rx);
+            telemetry.update();
+        }
     }
 }
