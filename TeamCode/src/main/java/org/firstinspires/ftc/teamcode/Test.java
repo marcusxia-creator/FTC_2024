62a80896fa1c2a244fa7a7fca578fbc4a89f96c8
Zirui-Ma <ziruima@outlook.com>
2024-09-29 13:22:31 -0600

    Add intake motor movement

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -1,47 +1,70 @@
-﻿package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Gamepad;
 
-public class Test {
+@TeleOp(name= "Test_Zirui", group = "OpMode")
+public class Test extends LinearOpMode {
     public DcMotor frontLeftMotor;
     public DcMotor frontRightMotor;
     public DcMotor backLeftMotor;
     public DcMotor backRightMotor;
 
-    public Gamepad gamepad1;
+    public DcMotor intakeMotor;
 
-    public Test(HardwareMap hardwaremap) {
-        frontLeftMotor = hardwaremap.get(DcMotor.class, "frontLeftMotor");
-        frontRightMotor = hardwaremap.get(DcMotor.class, "frontRightMotor");
-        backLeftMotor = hardwaremap.get(DcMotor.class, "backLeftMotor");
-        backRightMotor = hardwaremap.get(DcMotor.class, "backRightMotor");
+    @Override
+    public void runOpMode() {
+        frontLeftMotor = hardwareMap.get(DcMotor.class, "FL_Motor");
+        frontRightMotor = hardwareMap.get(DcMotor.class, "FR_Motor");
+        backLeftMotor = hardwareMap.get(DcMotor.class, "BL_Motor");
+        backRightMotor = hardwareMap.get(DcMotor.class, "BR_Motor");
 
-        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-    }
-    // Method to initialize the gamepad
-    public void initGamepad(Gamepad gamepad) {
-        this.gamepad1 = gamepad;
-    }
+        intakeMotor = hardwareMap.get(DcMotor.class, "In_Motor");
+        intakeMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER); // Reset the motor encoder
+        intakeMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER); // Turn the motor back on when we are done
 
-    public void drive() {
-        double y = -gamepad1.left_stick_y;
-        double x = gamepad1.left_stick_x;
-        double rx = gamepad1.right_stick_x;
+        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        int res = 2;
+        int position = Math.round(res * 537.7);
 
-        double frontLeftMotorPower = (y + x + rx) / denominator;
-        double frontRightMotorPower = (y - x + rx) / denominator;
-        double backLeftMotorPower = (y - x - rx) / denominator;
-        double backRightMotorPower = (y + x - rx) / denominator;
+        telemetry.addLine("Robot Initialized");
+        telemetry.update();
 
-        frontLeftMotor.setPower(frontLeftMotorPower);
-        frontRightMotor.setPower(frontRightMotorPower);
-        backLeftMotor.setPower(backLeftMotorPower);
-        backRightMotor.setPower(backRightMotorPower);
+        waitForStart();
+
+        while (opModeIsActive()) {
+            double y = -gamepad1.right_stick_y;
+            double x = gamepad1.right_stick_x;
+            double rx = gamepad1.left_stick_x;
+
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+
+            int position = intakeMotor.getCurrentPosition();
+
+            double frontLeftMotorPower = (y + x + rx * 0.4) / denominator;
+            double frontRightMotorPower = (y - x - rx * 0.4) / denominator;
+            double backLeftMotorPower = (y - x + rx * 0.4)/ denominator;
+            double backRightMotorPower = (y + x - rx * 0.4) / denominator;
+
+            frontLeftMotor.setPower(frontLeftMotorPower);
+            frontRightMotor.setPower(frontRightMotorPower);
+            backLeftMotor.setPower(backLeftMotorPower);
+            backRightMotor.setPower(backRightMotorPower);
+
+            telemetry.addLine("Robot Initialized");
+            telemetry.addData("Front Left Power", frontLeftMotor.getPower());
+            telemetry.addData("Front Right Power", frontRightMotor.getPower());
+            telemetry.addData("Back Left Power", backLeftMotor.getPower());
+            telemetry.addData("Back Right Power", backRightMotor.getPower());
+            telemetry.addData("Joystick x", x);
+            telemetry.addData("Joystick y", y);
+            telemetry.addData("Joystick rx", rx);
+            telemetry.addData("Intake Motor position", position);
+            telemetry.update();
+        }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestMovement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestMovement.java
@@ -11,24 +11,24 @@ public class TestMovement extends OpMode {
     @Override
     public void init(){
         // instantiate the test class.
-        robotDrive = new Test(hardwareMap);
+        //robotDrive = new Test(hardwareMap);
         // initialize gamepad
-        robotDrive.initGamepad(gamepad1);
+        //robotDrive.initGamepad(gamepad1);
         //telemetry the status of robot.
-        telemetry.addLine("Robot Initialized");
-        telemetry.update();
+        //telemetry.addLine("Robot Initialized");
+        //telemetry.update();
     }
 
     @Override
     public void loop(){
         //call the drive method to drive the robot
-        robotDrive.drive();
+        //robotDrive.drive();
         // Add telemetry for debugging
-        telemetry.addData("Front Left Power", robotDrive.frontLeftMotor.getPower());
-        telemetry.addData("Front Right Power", robotDrive.frontRightMotor.getPower());
-        telemetry.addData("Back Left Power", robotDrive.backLeftMotor.getPower());
-        telemetry.addData("Back Right Power", robotDrive.backRightMotor.getPower());
-        telemetry.update();
+        //telemetry.addData("Front Left Power", robotDrive.frontLeftMotor.getPower());
+        //telemetry.addData("Front Right Power", robotDrive.frontRightMotor.getPower());
+        //telemetry.addData("Back Left Power", robotDrive.backLeftMotor.getPower());
+        //telemetry.addData("Back Right Power", robotDrive.backRightMotor.getPower());
+        //telemetry.update();
 
     }
 }
