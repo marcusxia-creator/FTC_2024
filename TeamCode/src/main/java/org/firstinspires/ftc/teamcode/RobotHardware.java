71b1ea6be588bee787e473afb9ed0e0a6bf8436d
gxia <xiaguowei@gmail.com>
2024-10-13 23:33:24 -0600

    Update the Mapping with two slide motor and no servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
@@ -11,21 +11,35 @@ import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+/* config
+Control Hub:
+FL_motor 0; BL_motor 1, FR_motor_2, BR_motor_3
+Expansion Hub:
+VS_Slide_Motor_Right 0 , VS_Slide_Motor_Left 1
+ */
 
 public class RobotHardware {
     public MotorEx frontLeftMotor;
     public MotorEx backLeftMotor;
     public MotorEx frontRightMotor;
     public MotorEx backRightMotor;
+
+    /* not use
     public MotorEx leftodometry;
     public MotorEx rightodometry;
     public MotorEx centerodometry;
+    */
 
-    public MotorEx verticalSlideMotor1;
+    public MotorEx verticalSlideMotorLeft;
+    public MotorEx verticalSlideMotorRight;
 
-    public ServoEx intakeServo;
+    //public ServoEx intakeServo;
 
-    public SensorColor ColorSensor;
+    //public SensorColor ColorSensor;
 
     public HardwareMap hardwareMap;
 
@@ -41,33 +55,42 @@ public class RobotHardware {
         frontRightMotor = new MotorEx(hardwareMap, "FR_Motor", Motor.GoBILDA.RPM_435);
         backRightMotor = new MotorEx(hardwareMap, "BR_Motor", Motor.GoBILDA.RPM_435);
 
-        // set odometry
+        /*
+        // set odometry - not installed yet
         leftodometry = new MotorEx(hardwareMap, "FL_Motor");// set odometry
         rightodometry = new  MotorEx(hardwareMap,"BL_Motor");// set odometry
         centerodometry = new MotorEx(hardwareMap,"FR_Motor");// set odometry
+        */
 
         //set vertical slide Motor
-        verticalSlideMotor1 = new MotorEx(hardwareMap,"VS_Motor_1", Motor.GoBILDA.RPM_312);
+        verticalSlideMotorLeft = new MotorEx(hardwareMap,"VS_Motor_Left", Motor.GoBILDA.RPM_312);
+        verticalSlideMotorRight = new MotorEx(hardwareMap,"VS_Motor_Right", Motor.GoBILDA.RPM_312);
 
         //set intake servo
-        intakeServo = new SimpleServo(hardwareMap, "Intake_Servo",0,300, AngleUnit.DEGREES);
+        //intakeServo = new SimpleServo(hardwareMap, "Intake_Servo",0,300, AngleUnit.DEGREES);
 
-        // reset encoder
+        /*
+        // reset odometry encoder
         leftodometry.resetEncoder();
         rightodometry.resetEncoder();
         centerodometry.resetEncoder();
+        */
 
         //set motor mode and motor direction
         frontLeftMotor.setInverted(false);  // Reverse the left motor if needed
         backLeftMotor.setInverted(false);  // Reverse the left motor if needed
-        frontLeftMotor.setRunMode(Motor.RunMode.RawPower); // set motor mode
-        backLeftMotor.setRunMode(Motor.RunMode.RawPower); //set motor mode
-        frontRightMotor.setRunMode(Motor.RunMode.RawPower); // set motor mode
-        backRightMotor.setRunMode((Motor.RunMode.RawPower)); // set motor mode
+        frontLeftMotor.setRunMode(Motor.RunMode.VelocityControl); // set motor mode
+        backLeftMotor.setRunMode(Motor.RunMode.VelocityControl); //set motor mode
+        frontRightMotor.setRunMode(Motor.RunMode.VelocityControl); // set motor mode
+        backRightMotor.setRunMode((Motor.RunMode.VelocityControl)); // set motor mode
+
 
         // set slide motor mode and motor direction
-        verticalSlideMotor1.setRunMode(Motor.RunMode.PositionControl);
-        verticalSlideMotor1.stopAndResetEncoder();
+        verticalSlideMotorLeft.setInverted(true);
+        verticalSlideMotorLeft.setRunMode(Motor.RunMode.PositionControl);
+        verticalSlideMotorLeft.stopAndResetEncoder();
+        verticalSlideMotorRight.setRunMode(Motor.RunMode.PositionControl);
+        verticalSlideMotorRight.stopAndResetEncoder();
 
         // set robot motor power 0
         frontLeftMotor.set(0);
@@ -75,10 +98,10 @@ public class RobotHardware {
         backLeftMotor.set(0);
         backRightMotor.set(0);
         // reset encoders when stop
-        resetDriveEncoders();
+        //resetDriveEncoders();
 
         // set color sensor
-        ColorSensor = new SensorColor(hardwareMap, "Color_Sensor");
+        //ColorSensor = new SensorColor(hardwareMap, "Color_Sensor");
 
     }
 
@@ -95,15 +118,32 @@ public class RobotHardware {
 
         // set up REVImu
         imu = hardwareMap.get(IMU.class, "imu");
+
+        IMU.Parameters myIMUparameters = new IMU.Parameters(
+                new RevHubOrientationOnRobot(
+                        new Orientation(
+                                AxesReference.INTRINSIC,
+                                AxesOrder.ZYX,
+                                AngleUnit.DEGREES,
+                                -90,
+                                0,
+                                0,
+                                0  // acquisitionTime, not used
+                        )
+                )
+        );
         imu.initialize(
-                new IMU.Parameters(new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.UP, RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD))
+        myIMUparameters
+                //        new IMU.Parameters(new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.UP, RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD))
         );
     }
 
+    /*
     // reset odometry encoder
     public void resetDriveEncoders(){
         leftodometry.stopAndResetEncoder();
         rightodometry.stopAndResetEncoder();
         centerodometry.stopAndResetEncoder();
     }
+    */
 }
