868772c9b265caea9e933e6713799fbd881c747b
gxia <xiaguowei@gmail.com>
2024-10-13 22:33:42 -0600

    Update the Mapping with two slide motor and no servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlideTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlideTest.java
@@ -53,6 +53,8 @@ public class SlideTest extends LinearOpMode {
         // Sets the starting position of the arm to the down position
         armMotorLeft.setTargetPosition(armDownPosition);
         armMotorLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        armMotorRight.setTargetPosition(armDownPosition);
+        armMotorRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
         telemetry.addData("Robot Status", "Initialized");
         telemetry.update();
@@ -63,13 +65,19 @@ public class SlideTest extends LinearOpMode {
             if (gamepad1.a) {
                 armMotorLeft.setTargetPosition(armUpPosition);
                 armMotorLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                armMotorRight.setTargetPosition(armUpPosition);
+                armMotorRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
                 armMotorLeft.setPower(0.5);
+                armMotorRight.setPower(0.5);
             }
 
             // If the B button is pressed, lower the arm
             if (gamepad1.b) {
                 armMotorLeft.setTargetPosition(armDownPosition);
                 armMotorLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                armMotorRight.setTargetPosition(armDownPosition);
+                armMotorLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                armMotorLeft.setPower(0.5);
                 armMotorLeft.setPower(0.5);
             }
 
@@ -77,33 +85,41 @@ public class SlideTest extends LinearOpMode {
                 armNewPosition =armMotorLeft.getCurrentPosition() + 20;
                 armMotorLeft.setTargetPosition(armNewPosition);
                 armMotorLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                armMotorRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
                 armMotorLeft.setPower(0.5);
+                armMotorRight.setPower(0.5);
             }
 
             if (gamepad1.y && debounceTimer.seconds() > DEBOUNCE_TIME) {
                 armNewPosition = armMotorLeft.getCurrentPosition() - 20;
                 armMotorLeft.setTargetPosition(armNewPosition);
                 armMotorLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                armMotorRight.setTargetPosition(armNewPosition);
+                armMotorRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
                 armMotorLeft.setPower(0.5);
+                armMotorRight.setPower(0.5);
             }
 
             // Get the current position of the armMotor
-            double position = armMotorLeft.getCurrentPosition();
+            double Rposition = armMotorRight.getCurrentPosition();
+            double Lposition = armMotorLeft.getCurrentPosition();
 
             // Get the target position of the armMotor
-            double desiredPosition = armMotorLeft.getTargetPosition();
+            double RdesiredPosition = armMotorRight.getTargetPosition();
+            double LdesiredPosition = armMotorLeft.getTargetPosition();
 
             // Show the position of the armMotor on telemetry
-            double resolution = position/537.7;
+            double resolution = Rposition/537.7;
             double angle = resolution/360;
             double angle_normalized = resolution%360;
 
-            telemetry.addData("Encoder Position", position);
+            telemetry.addData("Right Encoder Position", Rposition);
+            telemetry.addData("Left Encoder Position", Lposition);
 
             // Show the target position of the armMotor on telemetry
-            telemetry.addData("Desired Position", desiredPosition);
+            telemetry.addData("Right Desired Position", RdesiredPosition);
+            telemetry.addData("Left Desired Position", LdesiredPosition);
 
-            telemetry.addData("target Position", position);
 
             telemetry.addData("resolution Position", resolution);
 
