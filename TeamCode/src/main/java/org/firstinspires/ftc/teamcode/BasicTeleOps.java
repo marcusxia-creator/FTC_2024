061e875c8abc9805203ba15c930f967ff9c3f7eb
marcusxia-creator <129916812+marcusxia-creator@users.noreply.github.com>
2024-10-16 13:19:16 -0600

    Update BasicTeleOps.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOps.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOps.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -11,6 +13,7 @@ public class BasicTeleOps extends OpMode {
     public GamepadEx gamepadCo2;
     public RobotDrive robotDrive;
     public FiniteMachineStateArm depositArmDrive;
+    private FtcDashboard dashboard;
 
     @Override
     public void init() {
@@ -21,6 +24,7 @@ public class BasicTeleOps extends OpMode {
         robotDrive.init(); // Initialize RobotDrive
         depositArmDrive = new FiniteMachineStateArm(robot, gamepadCo1, telemetry,0.5, .07, 1.0, 500, 1500); // Pass parameters as needed);
         depositArmDrive.init();
+        dashboard = FtcDashboard.getInstance();
         telemetry.addLine("-------------------");
         telemetry.addData("Status"," initialized Motors and Encoder and IMU and Arm Control");
         telemetry.addLine("-------------------");
@@ -32,6 +36,30 @@ public class BasicTeleOps extends OpMode {
     public void loop() {
         robotDrive.driveLoop(); // Use RobotDrive methods
         depositArmDrive.armLoop();
+
+        // Real-time telemetry data to Driver Station
+        telemetry.addData("Front Left Motor Power", robot.frontLeftMotor.getPower());
+        telemetry.addData("Front Right Motor Power", robot.frontRightMotor.getPower());
+        telemetry.addData("Back Left Motor Power", robot.backLeftMotor.getPower());
+        telemetry.addData("Back Right Motor Power", robot.backRightMotor.getPower());
+
+        telemetry.addData("Lift Motor Left Position", robot.liftMotorLeft.getCurrentPosition());
+        telemetry.addData("Lift Motor Right Position", robot.liftMotorRight.getCurrentPosition());
+
+        // Update telemetry for the driver station
+        telemetry.update();
+
+        //FTC DashBoard telemetry packet
+        TelemetryPacket packet = new TelemetryPacket();
+        packet.put("Front Left Motor Power", robot.frontLeftMotor.getPower());
+        packet.put("Front Right Motor Power", robot.frontRightMotor.getPower());
+        packet.put("Back Left Motor Power", robot.backLeftMotor.getPower());
+        packet.put("Back Right Motor Power", robot.backRightMotor.getPower());
+        packet.put("Lift Motor Left Position", robot.liftMotorLeft.getCurrentPosition());
+        packet.put("Lift Motor Right Position", robot.liftMotorRight.getCurrentPosition());
+        packet.put("IMU Heading", robot.getIMUHeading()); // Assuming there's a method to get IMU heading
+
+        dashboard.sentTelemetryPacket(packet);
     }
 
     public void stop() {;
